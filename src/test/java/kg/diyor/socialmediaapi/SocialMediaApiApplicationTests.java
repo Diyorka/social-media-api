fc818acb8f23275c8872b0e56093008462a6c53e
Diyor Umurzakov <specterplay2@gmail.com>
2023-08-15 20:15:40 +0600

    initialized project

---
+++ src/test/java/kg/diyor/socialmediaapi/SocialMediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.diyor.socialmediaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
