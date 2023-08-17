8c8b6a706458f90e3c2f3c0175a1f7b5f29a8d3d
= <diyorp2@mail.ru>
2023-08-17 23:06:21 +0600

    cors update

--- src/main/java/kg/diyor/socialmediaapi/config/SecurityConfiguration.java
+++ src/main/java/kg/diyor/socialmediaapi/config/SecurityConfiguration.java
@@ -14,6 +14,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -37,16 +39,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors
-                        .configurationSource(request -> {
-                            CorsConfiguration corsConfig = new CorsConfiguration();
-                            corsConfig.addAllowedOrigin("https://social-media-api-production-714d.up.railway.app"); // Разрешить только этот домен
-                            corsConfig.addAllowedMethod("*"); // Разрешить все HTTP методы
-                            corsConfig.addAllowedHeader("*"); // Разрешить все заголовки
-                            corsConfig.setAllowCredentials(true); // Разрешить кросс-доменные cookies и аутентификацию
-                            return corsConfig;
-                        })
-                )
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(WHITELISTED_ENDPOINTS).permitAll()
@@ -59,5 +52,15 @@ public class SecurityConfiguration {
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://social-media-api-production-714d.up.railway.app"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
